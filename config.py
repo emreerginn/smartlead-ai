c1102d11642089f293bfdde5d15c3026670c4f53
Emre Ergin <emreerginn39@gmail.com>
2026-09-20 01:23:59 +0300

    Lead listesi sifre korumali

--- config.py
+++ config.py
@@ -11,7 +11,9 @@ class Config:
     DATABASE_URL = os.environ.get('DATABASE_URL', 'leads.db')  # sqlite dosya yolu
     GROQ_API_KEY = os.environ.get('GROQ_API_KEY', '')
     AI_PROVIDER = os.environ.get('AI_PROVIDER', 'groq')
-    CORS_ORIGINS = os.environ.get('CORS_ORIGINS', '*')  
+    CORS_ORIGINS = os.environ.get('CORS_ORIGINS', '*')
+    DASH_USER = os.environ.get('DASH_USER', '')
+    DASH_PASS = os.environ.get('DASH_PASS', '') 
 
     BUSINESS_CONTEXT = """Sen CodeF Academy'nin yapay zeka asistanisin. CodeF Academy,
 Python, web gelistirme ve daha bircok alanda pratik yazilim kurslari sunan bir
